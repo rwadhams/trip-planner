a9347729f8e13a0d79e3779bd656182d9e2221c9
robert.wadhams <robert.wadhams@gmail.com>
2024-07-17 16:30:24 +0800

    indented nextDistance in reporting

--- TripPlannerApplication/src/main/groovy/com/wadhams/trip/planner/report/DailyTripPlannerReportService.groovy
+++ TripPlannerApplication/src/main/groovy/com/wadhams/trip/planner/report/DailyTripPlannerReportService.groovy
@@ -18,20 +18,24 @@ class DailyTripPlannerReportService {
 	def report(LocalDate startingDate, List<LocationNightsDTO> lnList, PrintWriter pw) {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern('EEE, MMM d, yyyy')
 		
+		int indent = 24
+		String nextDistanceIndent = ''
+		indent.times {nextDistanceIndent += ' '}
+		
 		LocalDate reportDate = startingDate
 		lnList.each {ln ->
 			//println ln
 			boolean firstLine = true
 			String nightText = (ln.nights == 1) ? 'night' : 'nights'
+			String firstText = "...: ${ln.nights} $nightText${(ln.campsite) ? ', Staying at: ' + ln.campsite : ''}${(ln.thingsToDo) ? ', ThingsToDo: ' + ln.thingsToDo : ''}"
 			ln.nights.times {
-				String firstText = "...: ${ln.nights} $nightText${(ln.campsite) ? ', Staying at: ' + ln.campsite : ''}${(ln.thingsToDo) ? ', ThingsToDo: ' + ln.thingsToDo : ''}"
-				pw.println "${dtf.format(reportDate)}\t${ln.location}${(firstLine) ? firstText : ''}"
+				pw.println "${dtf.format(reportDate).padRight(indent, ' ')}${ln.location}${(firstLine) ? firstText : ''}"
 				firstLine = false
 				reportDate = reportDate.next()
 			}
 			if (ln.nextDistance > 0) {
 				pw.println ''
-				pw.println "${ln.nextDistance} Kms"
+				pw.println "$nextDistanceIndent${ln.nextDistance} Kms"
 			}
 			pw.println ''
 		}
